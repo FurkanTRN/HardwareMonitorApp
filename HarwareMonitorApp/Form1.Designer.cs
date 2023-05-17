e6b79aaa38d0b4e4b40104ccec84f133709d43b7
furka <furka@MONSTER>
2023-05-17 21:35:26 +0300

    Add project files.

---
+++ HarwareMonitorApp/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace HarwareMonitorApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PortsComboBox = new System.Windows.Forms.ComboBox();
+            this.serialLabel = new System.Windows.Forms.Label();
+            this.ConnectButton = new System.Windows.Forms.Button();
+            this.DisconnectButton = new System.Windows.Forms.Button();
+            this.RefreshButton = new System.Windows.Forms.Button();
+            this.ConnectStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // PortsComboBox
+            // 
+            this.PortsComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.PortsComboBox.FormattingEnabled = true;
+            this.PortsComboBox.Location = new System.Drawing.Point(97, 12);
+            this.PortsComboBox.Name = "PortsComboBox";
+            this.PortsComboBox.Size = new System.Drawing.Size(156, 21);
+            this.PortsComboBox.Sorted = true;
+            this.PortsComboBox.TabIndex = 0;
+            this.PortsComboBox.SelectedIndexChanged += new System.EventHandler(this.PortsComboBox_SelectedIndexChanged);
+            // 
+            // serialLabel
+            // 
+            this.serialLabel.AutoSize = true;
+            this.serialLabel.Location = new System.Drawing.Point(12, 15);
+            this.serialLabel.Name = "serialLabel";
+            this.serialLabel.Size = new System.Drawing.Size(84, 13);
+            this.serialLabel.TabIndex = 1;
+            this.serialLabel.Text = "SERIAL PORT :";
+            // 
+            // ConnectButton
+            // 
+            this.ConnectButton.Location = new System.Drawing.Point(96, 39);
+            this.ConnectButton.Name = "ConnectButton";
+            this.ConnectButton.Size = new System.Drawing.Size(75, 23);
+            this.ConnectButton.TabIndex = 2;
+            this.ConnectButton.Text = "Connect";
+            this.ConnectButton.UseVisualStyleBackColor = true;
+            this.ConnectButton.Click += new System.EventHandler(this.ConnectButton_Click);
+            // 
+            // DisconnectButton
+            // 
+            this.DisconnectButton.Location = new System.Drawing.Point(177, 39);
+            this.DisconnectButton.Name = "DisconnectButton";
+            this.DisconnectButton.Size = new System.Drawing.Size(75, 23);
+            this.DisconnectButton.TabIndex = 3;
+            this.DisconnectButton.Text = "Disconnect";
+            this.DisconnectButton.UseVisualStyleBackColor = true;
+            this.DisconnectButton.Click += new System.EventHandler(this.DisconnectButton_Click);
+            // 
+            // RefreshButton
+            // 
+            this.RefreshButton.Location = new System.Drawing.Point(15, 39);
+            this.RefreshButton.Name = "RefreshButton";
+            this.RefreshButton.Size = new System.Drawing.Size(75, 23);
+            this.RefreshButton.TabIndex = 4;
+            this.RefreshButton.Text = "Refresh";
+            this.RefreshButton.UseVisualStyleBackColor = true;
+            this.RefreshButton.Click += new System.EventHandler(this.RefreshButton_Click);
+            // 
+            // ConnectStatus
+            // 
+            this.ConnectStatus.AutoSize = true;
+            this.ConnectStatus.Location = new System.Drawing.Point(259, 15);
+            this.ConnectStatus.Name = "ConnectStatus";
+            this.ConnectStatus.Size = new System.Drawing.Size(0, 13);
+            this.ConnectStatus.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(298, 72);
+            this.Controls.Add(this.ConnectStatus);
+            this.Controls.Add(this.RefreshButton);
+            this.Controls.Add(this.DisconnectButton);
+            this.Controls.Add(this.ConnectButton);
+            this.Controls.Add(this.serialLabel);
+            this.Controls.Add(this.PortsComboBox);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "HardwareMonitor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox PortsComboBox;
+        private System.Windows.Forms.Label serialLabel;
+        private System.Windows.Forms.Button ConnectButton;
+        private System.Windows.Forms.Button DisconnectButton;
+        private System.Windows.Forms.Button RefreshButton;
+        private System.Windows.Forms.Label ConnectStatus;
+    }
+}
+
---
+++ HarwareMonitorApp/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace HarwareMonitorApp
+{
+    public partial class Form1 : Form
+    {
+        private SerialPort serialPort;
+
+        public Form1()
+        {
+            InitializeComponent();
+            ConnectButton.Enabled = false;
+            DisconnectButton.Enabled = false;
+        }
+        void ScanPorts()
+        {
+            string[] ports = SerialPort.GetPortNames();
+            foreach (string port in ports)
+            {
+                PortsComboBox.Items.Add(port);
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ScanPorts();
+        }
+
+        private void RefreshButton_Click(object sender, EventArgs e)
+        {
+            PortsComboBox.Items.Clear();
+            ConnectButton.Enabled=false;
+            DisconnectButton.Enabled=false;
+            ScanPorts();
+        }
+
+        private void ConnectButton_Click(object sender, EventArgs e)
+        {
+            RefreshButton.Enabled = false;
+            DisconnectButton.Enabled=true;
+            InitializeSerialPort();
+            
+        }
+
+        private void PortsComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ConnectButton.Enabled = true;
+        }
+
+        private void DisconnectButton_Click(object sender, EventArgs e)
+        {
+            serialPort.Close();
+            ConnectStatus.Text = "";
+            RefreshButton.Enabled = true;
+            ConnectButton.Enabled = false;
+        }
+        private void InitializeSerialPort()
+        {
+            string portName = PortsComboBox.Text;
+            int baudRate = 9600;
+            Parity parity = Parity.None; 
+            int dataBits = 8;
+            StopBits stopBits = StopBits.One; 
+            serialPort = new SerialPort(portName, baudRate, parity, dataBits, stopBits);
+
+            try
+            {
+                serialPort.Open();
+                ConnectButton.Enabled = false;
+                RefreshButton.Enabled = false;
+                ConnectStatus.Text = "✔";
+            }
+            catch (Exception ex)
+            {
+                serialPort.Close();
+                MessageBox.Show("Port Connection Error", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
